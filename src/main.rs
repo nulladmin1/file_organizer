2ae8fd5bf0256b9524d3c27626d8f0dd7c0e40e3
nulladmin1 <shrey.deogade@protonmail.com>
2024-03-19 22:09:04 -0400

    Finished organize_files function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fs;
 use std::env::current_dir;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 enum Subdirectories {
     Archives,
@@ -12,11 +12,23 @@ enum Subdirectories {
     Videos,
 }
 
-struct DirFileMap<'a>{
-    subdirectory: &'a Subdirectories,
-    filetypes: Vec<&'a str>,
+impl AsRef<Path> for Subdirectories {
+    fn as_ref(&self) -> &Path {
+        match self {
+            Subdirectories::Archives => Path::new("Archives"),
+            Subdirectories::Code => Path::new("Code"),
+            Subdirectories::Documents => Path::new("Documents"),
+            Subdirectories::Music => Path::new("Music"),
+            Subdirectories::Pictures => Path::new("Pictures"),
+            Subdirectories::Videos => Path::new("Videos"),
+        }
+    }
 }
 
+struct DirFileMap<'a>{
+    subdirectory: &'a Subdirectories,
+    filetypes: &'a [&'a str],
+}
 
 struct FileMatch<'a> {
     directory: &'a Subdirectories,
@@ -32,20 +44,37 @@ impl <'a> FileMatch<'a> {
     }
 }
 
-fn get_files(directory: PathBuf, master_dir_file_map: &[DirFileMap<'static>]) -> Result<Vec<FileMatch<'static>>, Box<dyn Error>> {
+const DIR_FILE_MAP: &[DirFileMap<'static>] = &[
+    DirFileMap { subdirectory: &Subdirectories::Archives, filetypes: &["tar", "gz", "xz", "bz2", "zip", "7z", "rar", "dmg,"] },
+    DirFileMap { subdirectory: &Subdirectories::Music, filetypes: &["mp3", "mp4a", "wav"] },
+    DirFileMap { subdirectory: &Subdirectories::Pictures, filetypes: &["jpg", "jpeg", "gif", "bmp", "pmg", "svg", "heif"] },
+    DirFileMap { subdirectory: &Subdirectories::Videos, filetypes: &["mp4", "mkv", "mov", ] },
+    DirFileMap { subdirectory: &Subdirectories::Documents, filetypes: &["pdf", "epub", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "odt", "rtf", "md", ] },
+    DirFileMap { subdirectory: &Subdirectories::Code, filetypes: &["py", "rs", "rb", "html", "htm", "js", "jar", "java", "c", "cpp", "toml", "json", "yaml", "tex", "cs", "nix", "pl", "h", "hpp", "css", "php", "swift", "go", "lua", "ts", "scss", "sass", "dart", "sql", "ini", "sh", "bash", "zsh", "ps1", "fish", "r", "yml", "bat", "cmd", "asm"] }
+];
+
+fn get_files(directory: PathBuf) -> Result<Vec<FileMatch<'static>>, Box<dyn Error>> {
     let directory_files = fs::read_dir(&directory)?;
     let mut file_match_maps: Vec<FileMatch> = Vec::new();
 
     for directory_file in directory_files {
         let directory_file = directory_file?;
         if directory_file.path().is_file() {
-            let file_extension = match directory_file.path().extension().and_then(|e| e.to_str()) {
+            let path = directory_file.path();
+            let path_extension = path.extension();
+
+            let file_extension = match path_extension {
+                Some(extension) => {
+                    if let Some(extension_str) = extension.to_str() {
+                        extension_str
+                    } else {
+                        continue
+                    }
+                }
                 None => {continue}
-                Some(extension) => {extension.to_owned()}
             };
 
-            for dir_file_map in master_dir_file_map {
-                // let file_extension_str = file_extension;
+            for dir_file_map in DIR_FILE_MAP {
                 if dir_file_map.filetypes.contains(&&*file_extension) {
                     file_match_maps.push(FileMatch::new(
                         dir_file_map.subdirectory,
@@ -59,23 +88,31 @@ fn get_files(directory: PathBuf, master_dir_file_map: &[DirFileMap<'static>]) ->
     Ok(file_match_maps)
 }
 
-fn organize_files(filematches: Vec<FileMatch<'static>>, master_dir_file_map: &[DirFileMap<'static>]) {
-    
+fn organize_files(filematches: &Vec<FileMatch<'static>>) -> Result<(), Box<dyn Error>> {
+    for subdirectory in DIR_FILE_MAP {
+        fs::create_dir_all(subdirectory.subdirectory)?;
+    }
+    for file_match in filematches {
+        println!("{:?}", file_match.file_path);
+
+        let subdirectory_path = Path::new(file_match.directory.as_ref());
+        let file_name = match file_match.file_path.path().file_name() {
+            Some(file_name) => file_name.to_owned(),
+            None => continue,
+        };
+        let destination_file = subdirectory_path.join(&file_name);
+
+        fs::rename(&file_match.file_path.path(), &destination_file)?;
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let current_dir = current_dir()?;
-    let dir_file_map: &[DirFileMap<'static>] = &[
-    DirFileMap { subdirectory: &Subdirectories::Archives, filetypes: vec!["tar", "gz", "xz", "bz2", "zip", "7z", "rar", "dmg,"] },
-    DirFileMap { subdirectory: &Subdirectories::Music, filetypes: vec!["mp3", "mp4a", "wav"] },
-    DirFileMap { subdirectory: &Subdirectories::Pictures, filetypes: vec!["jpg", "jpeg", "gif", "bmp", "pmg", "svg", "heif"] },
-    DirFileMap { subdirectory: &Subdirectories::Videos, filetypes: vec!["mp4", "mkv", "mov", ] },
-    DirFileMap { subdirectory: &Subdirectories::Documents, filetypes: vec!["pdf", "epub", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "odt", "rtf", "md", ] },
-    DirFileMap { subdirectory: &Subdirectories::Documents, filetypes: vec!["py", "rs", "rb", "html", "htm", "js", "jar", "java", "c", "cpp", "toml", "json", "yaml", "tex", "cs", "nix", "pl", "h", "hpp", "css", "php", "swift", "go", "lua", "ts", "scss", "sass", "dart", "sql", "ini", "sh", "bash", "zsh", "ps1", "fish", "r", "yml", "bat", "cmd", "asm"] }
-];
-    let files_to_organize = match get_files(current_dir, dir_file_map) {
-        Ok(files) => files,
-        Err(err) => panic!("Failed to get files: {:?}", err)
-    };
+    let files_to_organize = get_files(current_dir)?;
+
+    organize_files(&files_to_organize)?;
+
     Ok(())
 }
